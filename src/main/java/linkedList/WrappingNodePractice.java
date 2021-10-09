8efe7de844e0af96c76345619500647d10a234e8
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-10-09 12:08:44 +0900

    Create WrappingNodePractice.java
    기존 Node 클래스만 사용하는 경우 삭제 시 주소값을 참조하는 로직 이있을 경우 문제가 될 수 있다. 그리고 첫번째 값도 못지우는 문제가 있음

---
+++ src/main/java/linkedList/WrappingNodePractice.java
@@ -0,0 +1,52 @@
+package linkedList;
+
+class LinkedList {
+	Node header;
+	
+	static class Node {
+		int data;
+		Node next = null;
+	}
+	
+	LinkedList(){
+		header = new Node();
+	}
+	
+	void append(int d) {
+		Node end = new Node();
+		Node n = header;
+		while (n.next != null) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+	
+	void delete(int d) {
+		Node n = header;
+		while (n.next != null) {
+			if (n.next.data == d) {
+				n.next = n.next.next;
+			}else {
+				n = n.next;
+			}
+		}
+	}
+	
+	void retrieve() {
+		Node n = header.next;
+		while(n.next != null) {
+			System.out.print(n.data + " -> ");
+			n = n.next;
+		}
+		System.out.println(n.data);
+	}
+}
+
+public class WrappingNodePractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
