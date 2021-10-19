9c13a07e44b7e536f3579721f01cc6aa0b882d10
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2021-10-19 23:48:17 +0900

    Create LastSearchTest.java

---
+++ src/main/java/linkedList/LastSearchTest.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class LastSearchTest {
+	
+	// linked list의 마지막 부터 k번째 값을 가져오기
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
